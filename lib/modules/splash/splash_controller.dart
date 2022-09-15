d31faad9178a61672f795a5fe16a14a8a90344ab
hasanoz07 <hasanozz2002@gmail.com>
2022-09-15 19:10:15 +0300

    firebase configure ok

--- lib/modules/splash/splash_controller.dart
+++ lib/modules/splash/splash_controller.dart
@@ -5,6 +5,6 @@ class SplashController extends GetxController {
   void onReady() async {
     super.onReady();
     await Future.delayed(Duration(milliseconds: 3000));
-    Get.toNamed(Routes.HOMEPAGE);
+    Get.toNamed(Routes.LOGIN);
   }
 }
