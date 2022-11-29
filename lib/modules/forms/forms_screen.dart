02da5172c0366c328cac0493eeb10744921321e5
STF16 <stf16@STF16s-iMac.local>
2022-11-29 16:39:19 +0300

    bug-fixed

--- lib/modules/forms/forms_screen.dart
+++ lib/modules/forms/forms_screen.dart
@@ -23,73 +23,86 @@ class FormsScreen extends GetView<FormsController> {
           centerTitle: true,
           title: Text(
             AppStrings.formsTitle,
-            style: GoogleFonts.inconsolata(fontSize: 18.sp, fontWeight: FontWeight.w600),
+            style: GoogleFonts.inconsolata(
+                fontSize: 18.sp, fontWeight: FontWeight.w600),
           ),
         ),
-        body: StreamBuilder<QuerySnapshot>(
-          //Fetching data from the documentId specified of the student
-          stream: controller.stream,
-          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            //Error Handling conditions
-            if (snapshot.hasError) {
-              return Text("Something went wrong");
-            }
+        body: ListView(
+          children: [
+            StreamBuilder<QuerySnapshot>(
+              //Fetching data from the documentId specified of the student
+              stream: controller.stream,
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                //Error Handling conditions
+                if (snapshot.hasError) {
+                  return Text("Something went wrong");
+                }
 
-            //Data is output to the user
-            if (snapshot.hasData) {
-              return Padding(
-                padding: AppPadding.projectPadding,
-                child: Column(
-                  children: [
-                    Container(
-                      width: 100.w,
-                      height: 85.h,
-                      child: ListView.separated(
-                          itemBuilder: (context, index) {
-                            return Bounceable(
-                              onTap: () async {
-                                final Uri _url = Uri.parse(snapshot.data?.docs[index]["formUrl"]);
+                //Data is output to the user
+                if (snapshot.hasData) {
+                  return Padding(
+                    padding: AppPadding.projectPadding,
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 100.w,
+                          height: 85.h,
+                          child: ListView.separated(
+                              itemBuilder: (context, index) {
+                                return Bounceable(
+                                  onTap: () async {
+                                    final Uri _url = Uri.parse(
+                                        snapshot.data?.docs[index]["formUrl"]);
 
-                                if (!await launchUrl(_url)) {
-                                  throw 'Could not launch $_url';
-                                }
-                              },
-                              child: Material(
-                                borderRadius: BorderRadius.circular(16.sp),
-                                elevation: 16,
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                      color: AppColors.lakeView, borderRadius: BorderRadius.circular(16.sp)),
-                                  width: 100.w,
-                                  height: 12.h,
-                                  child: Center(
-                                    child: Text(snapshot.data?.docs[index]["formTitle"],
-                                        style: GoogleFonts.inconsolata(
-                                            color: AppColors.white, fontSize: 20.sp, fontWeight: FontWeight.w600)),
+                                    if (!await launchUrl(_url)) {
+                                      throw 'Could not launch $_url';
+                                    }
+                                  },
+                                  child: Material(
+                                    borderRadius: BorderRadius.circular(16.sp),
+                                    elevation: 16,
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                          color: AppColors.lakeView,
+                                          borderRadius:
+                                              BorderRadius.circular(16.sp)),
+                                      width: 100.w,
+                                      height: 12.h,
+                                      child: Center(
+                                        child: Text(
+                                            snapshot.data?.docs[index]
+                                                ["formTitle"],
+                                            style: GoogleFonts.inconsolata(
+                                                color: AppColors.white,
+                                                fontSize: 20.sp,
+                                                fontWeight: FontWeight.w600)),
+                                      ),
+                                    ),
                                   ),
-                                ),
-                              ),
-                            );
-                            /*
-                                foodName: snapshot.data?.docs[index]["foodName"],
-                                foodWeight: snapshot.data?.docs[index]["foodWeight"]);
-                                */
-                          },
-                          separatorBuilder: (context, index) => SizedBox(
-                                height: 3.h,
-                              ),
-                          itemCount: snapshot.data?.docs.length ?? 0),
-                    )
-                  ],
-                ),
-              );
-            }
+                                );
+                                /*
+                                    foodName: snapshot.data?.docs[index]["foodName"],
+                                    foodWeight: snapshot.data?.docs[index]["foodWeight"]);
+                                    */
+                              },
+                              separatorBuilder: (context, index) => SizedBox(
+                                    height: 3.h,
+                                  ),
+                              itemCount: snapshot.data?.docs.length ?? 0),
+                        )
+                      ],
+                    ),
+                  );
+                }
 
-            return Center(
-                child: CircularProgressIndicator(
-              color: AppColors.white,
-            ));
-          },
+                return Center(
+                    child: CircularProgressIndicator(
+                  color: AppColors.white,
+                ));
+              },
+            ),
+          ],
         ));
   }
 }
